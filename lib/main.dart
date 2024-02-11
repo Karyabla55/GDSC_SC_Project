4d0526fe33debdb6603e823eeb194e4a356ca073
teomanata74 <teomanata74@gmail.com>
2024-02-11 22:58:27 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+//selamlar
 //burası yorum satırı
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -22,6 +25,9 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
+ 
+
   final String title;
 
   @override
@@ -33,19 +39,26 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
+     
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
+        
+       
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
         title: Text(widget.title),
       ),
       body: Center(
+       
         child: Column(
+          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -62,7 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),
+      ), 
     );
   }
 }
